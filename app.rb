b764f791abdf4adb5df9faa0d29232017e6e481b
Thomas Ferris <thomaskeithferris@gmail.com>
2019-04-27 13:12:02 -0400

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,11 +1,11 @@
 require "sqlite3"
 
 
-# Instantiate a constant variable, DB, usable in all your files
+# Instantiate a constant variable, DB, usable in all files
 dir = File.dirname(__FILE__)
 DB = SQLite3::Database.new(File.join(dir, "db/posts.db"))
 
-# Require all the ruby files you have created in `app`
+# Require all the ruby files created in `app`
 Dir[File.join(dir, "app/**/*.rb")].each { |file| require file }
 
 # Launch the app!
